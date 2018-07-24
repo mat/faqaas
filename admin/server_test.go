bfb28bdfcfd02bda88356b3f6ad099d44131a25b
Matthias Luedtke <matthias.luedtke@gmail.com>
2018-07-24 13:54:27 +0200

    Add TestGetAdminLogin

--- admin/server_test.go
+++ admin/server_test.go
@@ -10,20 +10,7 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
-func doRequest(method, uri string, body *bytes.Buffer, handle httprouter.Handle) (*httptest.ResponseRecorder, error) {
-	resp := httptest.NewRecorder()
-	req, err := http.NewRequest(method, uri, body)
-	if err != nil {
-		return nil, err
-	}
-
-	router := httprouter.New()
-	router.Handle(method, uri, handle)
-	router.ServeHTTP(resp, req)
-	return resp, nil
-}
-
-func TestGetStuff(t *testing.T) {
+func TestGetAdminIndex(t *testing.T) {
 	body := bytes.NewBufferString("hello")
 	handle := getAdmin
 	resp, err := doRequest("GET", "/", body, handle)
@@ -35,6 +22,33 @@ func TestGetStuff(t *testing.T) {
 	expectBodyContains(t, resp, `<a href="/admin/faqs">Found</a>`)
 	expectStatus(t, resp, 302)
 	expectHeader(t, resp, "Location", "/admin/faqs")
+}
+
+func TestGetAdminLogin(t *testing.T) {
+	body := bytes.NewBufferString("")
+	handle := getAdminLogin
+	resp, err := doRequest("GET", "/admin/login", body, handle)
+
+	if err != nil {
+		panic(err)
+	}
+
+	expectBodyContains(t, resp, `<title>Admin / Login</title>`)
+	expectBodyContains(t, resp, `<form action="/admin/login" method="post"`)
+	expectStatus(t, resp, 200)
+}
+
+func doRequest(method, uri string, body *bytes.Buffer, handle httprouter.Handle) (*httptest.ResponseRecorder, error) {
+	resp := httptest.NewRecorder()
+	req, err := http.NewRequest(method, uri, body)
+	if err != nil {
+		return nil, err
+	}
+
+	router := httprouter.New()
+	router.Handle(method, uri, handle)
+	router.ServeHTTP(resp, req)
+	return resp, nil
 }
 
 func expectStatus(t *testing.T, resp *httptest.ResponseRecorder, expected int) {
