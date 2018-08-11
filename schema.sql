298e6de28cf902570d5910469f1b7efdeafa3294
Matthias Luedtke <matthias.luedtke@gmail.com>
2018-08-11 14:40:39 +0200

    Clean up schema.sql

--- schema.sql
+++ schema.sql
@@ -25,24 +25,8 @@ SELECT faq_texts.id,
        setweight(to_tsvector('simple', faq_texts.answer), 'B') as document
 --       setweight(to_tsvector('simple', author.name), 'C') ||
 --       setweight(to_tsvector('simple', coalesce(string_agg(tag.name, ' '))), 'A') as document
-FROM faq_texts
---JOIN author ON author.id = post.author_id
---JOIN posts_tags ON posts_tags.post_id = posts_tags.tag_id
---JOIN tag ON tag.id = posts_tags.tag_id
---GROUP BY post.id, author.id
-;
+FROM faq_texts;
 
 CREATE INDEX idx_fts_search ON search_index USING gin(document);
 
 REFRESH MATERIALIZED VIEW search_index;
-
---SELECT DISTINCT * FROM (
---	SELECT faq_texts.faq_id
---	FROM search_index
---	JOIN faq_texts ON search_index.id = faq_texts.id
---	WHERE document @@ plainto_tsquery('simple', 'auto')
---	ORDER BY ts_rank(document, plainto_tsquery('simple', 'auto')) DESC
---) faqids;
-
---DROP MATERIALIZED VIEW search_index;
--- INSERT INTO faq_texts (faq_id,locale,question,answer) VALUES (10,'de', 'Wer hat an der Uhr gedreht?', 'Paulchen Panter');
